97bfed28113e561c149f00d2fe1902329ecdf0bb
hamada.alosh <mohamad.75.alosh@gmail.com>
2022-05-07 21:04:09 +0100

    I hate VSC

--- src/main/main.py
+++ src/main/main.py
@@ -5,10 +5,10 @@ from re import finditer
 
 players = []
 players_in_round = []
-
+small_blind = 10
+big_blind = 20
 table = []
 pot = 0
-
 ranks = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 suits = ["Clubs","Hearts","Diamonds","Spades"]
 deck = []
@@ -21,7 +21,7 @@ def resetDeck():
     random.shuffle(deck)
     
 def addPlayer():
-    #[Name, Money, Card1, Card2, currentpot]
+    #[Name, Money, Card1, Card2, currentbid]
     player = []
     player.append(input("Please enter your name: "))
     player.append(int(input("Please enter your buy in £")))
@@ -31,11 +31,18 @@ def addPlayer():
     return player
 
 def getPlayers():
-    not_enough = True
-    while(not_enough):
-        players.append(addPlayer())
-        if input("Is that all players? Y/N").upper() == "Y":
-            not_enough = False
+    #This code is for getting user input. temp comment to save time
+    # not_enough = True
+    # while(not_enough):
+    #     players.append(addPlayer())
+    #     if input("Is that all players? Y/N").upper() == "Y":
+    #         not_enough = False
+    
+    #temp code
+    players.append(["Hammy", 100, "", "", 0])
+    players.append(["Matthew", 100, "", "", 0])
+    players.append(["George", 100, "", "", 0])
+    players.append(["Kamil", 100, "", "", 0])
     print("Here is a list of all players")
     print(players)
     print("Lets begin")
@@ -47,14 +54,19 @@ def dealCards():
     for playa in players:
         playa[3] = deck.pop(0)
 
+def blinds():
+    players_in_round[0][4]=small_blind
+    players_in_round[1][4]=big_blind
+
 def playRound(first):
     current_pot = 0
-    current_bid = 0
+    current_bid = 20
     table.append(deck.pop(0))
     if first:
         table.append(deck.pop(0))
         table.append(deck.pop(0))
     done = False
+    message = " how much would you like to bid?"
     while(not(done)):
         for playa in players_in_round:
             print("Cards on the table")
@@ -63,38 +75,49 @@ def playRound(first):
             print(playa)
             finished = False
             while (not(finished)):
-                i = input("Dear " + playa[0] + " how much would you like to bid?")
-                if i.upper() == "F":
-                    players_in_round.remove(playa)
-                    finished = True
-                else:
-                    i = int(i)
-                    if i > playa[1]:
-                        print("you do not have enough money to bid that much")
-                    elif i < current_bid:
-                        print("You must bid more than" + str(current_bid))
-                    else:
-                        playa[1] = playa[1]- i
-                        current_bid = i
-                        current_pot = current_pot+ current_bid
+                if playa[4]<current_bid:
+                    i = input("Dear " + playa[0] + message)
+                    if i.upper() == "F":
+                        players_in_round.remove(playa)
                         finished = True
-            print("player " + playa[0] + " bid " + str(current_bid) )
-        
+                    else:
+                        i = int(i)
+                        if i > playa[1]:
+                            print("you do not have enough money to bid that much")
+                        elif i < current_bid:
+                            print("You must bid more than" + str(current_bid))
+                        else:
+                            playa[1] = playa[1]- i
+                            current_bid = i
+                            playa[4] = i
+                            current_pot = current_pot+ current_bid
+                            finished = True
+                else:
+                    finished = True
+                    print("player " + playa[0] + " bid " + str(current_bid) )
+        for playa in players_in_round:
+            if(playa[4]!= current_bid):
+                done = False
+                break
+            else:
+                done = True
             
     print("Round over!")
-    pot = pot + current_pot
+    pot += current_pot
         
             
 
 
 if __name__ == '__main__':
     while(True):
+        pot = 0
         deck = []
         resetDeck()
         getPlayers()
         dealCards()
         print(players)
         players_in_round = players
+        blinds()
         playRound(True)
         print(pot)
         playRound(False)
