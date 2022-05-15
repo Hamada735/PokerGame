5f8aa90bd2bdddaf7720b1292c0032c921b1561b
Matthew Polland <matthew.polland.2020@uni.strath.ac.uk>
2022-05-15 16:58:03 +0100

    created win check methods

--- src/main/main.py
+++ src/main/main.py
@@ -5,7 +5,6 @@ import random
 from re import finditer
 
 players = []
-players_in_round = []
 small_blind = 10
 big_blind = 20
 table = []
@@ -48,10 +47,11 @@ def getPlayers():
     #            not_enough = False
     
     #temp code, uncomment to save time
-    players.append(["Hammy", 100, "", "", 0])
-    players.append(["Matthew", 100, "", "", 0])
-    players.append(["George", 100, "", "", 0])
-    players.append(["Kamil", 100, "", "", 0])
+
+    players.append(["Hammy", 100, "", "", 0, True])
+    players.append(["Matthew", 100, "", "", 0, True])
+    players.append(["George", 100, "", "", 0, True])
+    players.append(["Kamil", 100, "", "", 0, True])
 
     print("Here is a list of all players")
     print("\n")
@@ -73,68 +73,117 @@ def dealCards():
     #this is two loops to work around the poker dealing conventions
     for playa in players:
         playa[3] = deck.pop(0)
+    print("cards dealt")
 
 def blinds():
-    players_in_round[0][4]=small_blind
-    players_in_round[1][4]=big_blind
+    players[0][4]=small_blind
+    print("player ", players[0][0] ," is small blind")
+    players[1][4]=big_blind
+    
 
 def playRound(first):
     global pot
     current_pot = 0
     current_bid = 20
-    table.append(deck.pop(0))
     if first:
         table.append(deck.pop(0))
         table.append(deck.pop(0))
+        table.append(deck.pop(0))
+    else:
+        table.append(deck.pop(0))
+        print(table)
     done = False
     message = " how much would you like to bid? "
     while(not done):
-        for playa in players_in_round:
-            if not first:
-                print("Cards on the table")
-                print(table)
-                print("\n")
-            playerToDisplay(playa)
-            finished = False
-            while (not finished):
-                if playa[4]<current_bid:
-                    i = input("Dear " + playa[0] + message)
-                    if i.upper() == "F":
-                        players_in_round.remove(playa)
-                        finished = True
-                    else:
-                        try:
-                            i = int(i)
-                        except:
-                            print("Error: Enter an integer value or 'F' to fold") 
-                            continue
-                        if i > playa[1]:
-                            print("You do not have enough money to bid that much")
-                        elif i < current_bid:
-                            print("You must bid more than " + str(current_bid))
-                        else:
-                            playa[1] = playa[1]- i
-                            current_bid = i
-                            playa[4] = i
-                            current_pot = current_pot+ current_bid
-                            finished = True
-                else:
-                    finished = True
-                    print(playa[0] + " bid " + str(current_bid))
+        for playa in players:
+            if playa[5] == True:
+                if not first:
+                    print("Cards on the table")
+                    print(table)
                     print("\n")
-        for playa in players_in_round:
-            if(playa[4]!= current_bid):
-                done = False
-                break
-            else:
-                done = True
+                playerToDisplay(playa)
+                finished = False
+                while (not finished):
+                    if playa[4]<current_bid:
+                        i = input("Dear " + playa[0] + message)
+                        if i.upper() == "F":
+                            playa[5] = False
+                            finished = True
+
+                            checkWinner()
+                            
+                        else:
+                            try:
+                                i = int(i)
+                            except:
+                                print("Error: Enter an integer value or 'F' to fold") 
+                                continue
+                            if i > playa[1]:
+                                print("You do not have enough money to bid that much")
+                            elif i < current_bid:
+                                print("You must bid more than ", current_bid)
+                            else:
+                                current_bid = i
+                                playa[4] = i
+                                current_pot = current_pot+ current_bid
+                                finished = True
+                    else:
+                        finished = True
+                        print(playa[0] + " bid " + str(current_bid))
+                        print("\n")
+                    
+        for playa in players:
+            if playa[5] == True:
+                if(playa[4]!= current_bid):
+                    done = False
+                    break
+                else:
+                    done = True
             
     print("Round over!")
 
-    for playa in players_in_round:
+    for playa in players:
+        playa[1] -= playa[4]
         playa[4] = 0
-
+    current_bid = 0
     pot += current_pot
+
+def resetPlayers():
+    for player in players:
+        player[4] = 0
+        player[5] = True
+
+def checkWinner():
+    nonFoldPlayers = []
+    for player in players:
+        if player[5] == True:
+            nonFoldPlayers.append(player)
+    if len(nonFoldPlayers) == 1:
+        print("Player ", nonFoldPlayers[0][0], " has won!")
+        return nonFoldPlayers[0]
+    return
+
+        
+def sort(hand):
+    handy = []
+
+
+def whoWon():      # < gotta check hand rankings :(
+    hands=[]
+    for player in players:
+        if player[5] == True:
+            hands.append([table[0],table[1],table[2],table[3],table[4],player[2],player[3]])
+    for hand in hands:
+        hand = sort(hand)
+
+    won("bruh")
+            
+def won(name):
+    for player in players:
+        if player[0] == name:
+            player[1]+= pot
+            exit()
+            
 
 
 if __name__ == '__main__':
@@ -145,7 +194,6 @@ if __name__ == '__main__':
         resetDeck()
         getPlayers()
         dealCards()
-        players_in_round = players
         blinds()
         playRound(True)
         print(pot)
@@ -153,7 +201,7 @@ if __name__ == '__main__':
         print(pot)
         playRound(False)
         print(pot)
+        whoWon()
+        resetPlayers()
         input("")
-
-
-# will work on this more after I/we hunt some women - Hammy and/or George
+        
