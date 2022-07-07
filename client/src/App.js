3a47f754554dbc9e58eed012917fe8137116583c
hamada.alosh <mohamad.75.alosh@gmail.com>
2022-07-07 19:16:54 +0100

    Started laying ground work for Front-End

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React, {useState, useEffect} from 'react'
+import styles from './App.css'; 
 
 function App() {
 
-  const [table,setTable] = useState([{}])
+  
 
+  return (
+    <>
+    <h1>Welcome to the Python Poker Program</h1>
+    <div className={styles.row}>
+    <div className={styles.column}>
+    <Table/>
+    </div>
+    </div>
+    <button>Load table</button>
+    </>
+  )
+}
+
+function Table() {
+  const [table,setTable] = useState([{}])
   useEffect(() => {
     fetch("/table").then(
       res => res.json()
@@ -17,21 +33,30 @@ function App() {
   )
 
   return (
-    
+    <>
     <div>
       {(typeof table.cards === 'undefined') ? (
-        <p>No cards dealt</p>
+        <p>Error in database UwU</p>
       ):(
-        table.cards.map((card,i) => (
-          <img className='column' key={i} src={`https://tekeye.uk/playing_cards/images/svg_playing_cards/fronts/${card}.svg`}/>
-        ))
+        <>
+        <img className={styles.column}src='https://tekeye.uk/playing_cards/images/svg_playing_cards/backs/astronaut.svg'/>
+        <img className={styles.column}src='https://tekeye.uk/playing_cards/images/svg_playing_cards/backs/astronaut.svg'/>
+        <img className={styles.column}src='https://tekeye.uk/playing_cards/images/svg_playing_cards/backs/astronaut.svg'/>
+        <img className={styles.column}src='https://tekeye.uk/playing_cards/images/svg_playing_cards/backs/astronaut.svg'/>
+        <img className={styles.column}src='https://tekeye.uk/playing_cards/images/svg_playing_cards/backs/astronaut.svg'/>
+        </>
       )
       }
-
     </div>
+    </>
   )
 }
+
+
 
 export default App
 
 ///https://tekeye.uk/playing_cards/images/svg_playing_cards/backs/astronaut.svg
+///(table.cards.map((card,i) => (
+///  <img className='column' key={i} src={`https://tekeye.uk/playing_cards/images/svg_playing_cards/fronts/${card}.svg`}/>
+///  )))
